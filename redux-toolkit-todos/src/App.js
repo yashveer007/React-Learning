871759d1380bc2d1e3fa2ad99bb5d9e2a05ac454
yashveer007 <yashveersingyc@gmail.com>
2024-06-08 13:22:57 +0530

    second commit

--- redux-toolkit-todos/src/App.js
+++ redux-toolkit-todos/src/App.js
@@ -1,9 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import AddTodo from './components/AddTodo';
+import Todos from './components/Todos';
 
 function App() {
   return (
     <>
+    <AddTodo />
+    <Todos />
     </>
   );
 }
--- redux-toolkit-todos/src/app/Store.js
+++ redux-toolkit-todos/src/app/Store.js
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
+import todoReducer from '../feature/todo/TodoSlice';
 
 export const Store = configureStore({
-
+    reducer : todoReducer
 })
